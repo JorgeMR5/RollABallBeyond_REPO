8ed1822aa15c9b96d0a2c54a31d1b5297c1bf3e5
JorgeMR5 <144338977+JorgeMR5@users.noreply.github.com>
2024-10-17 12:50:23 +0200

    Primera subida

---
+++ Juego_RollABall/Assets/Prototype_Root/RaB_Base/Scripts/PlayerInteraction.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Librería para poder referenciar elementos de User Interface
+using TMPro; //Librería para poder referenciar elementos de Text Mesh Pro
+
+public class PlayerInteraction : MonoBehaviour
+{
+
+    [Header("UI References")]
+    public TMP_Text pointsText; //Ref al texto de UI que quiero que cambie dinámicamente según los puntos del player
+
+    [Header("Scene Management")]
+    public SceneChanger sceneManagerScript;
+    public int sceneToLoad;
+
+
+    [Header("Point System Parameters")]
+    //Variables para definir los puntos del jugador
+    public int currentPoints;
+    public int winPoints;
+    public GameObject winGoal;
+
+    [Header("Respawn Parameters")]
+    public Transform respawnPoint;
+    public float respawnFallLimit;
+
+    private void Update()
+    {
+        if (currentPoints < 0) { currentPoints = 0; }
+        if (transform.position.y <= respawnFallLimit) { Respawn(); }
+        if (currentPoints >= winPoints) { winGoal.SetActive(true); }
+        UIUpdater();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            currentPoints += 1;
+            other.gameObject.SetActive(false);
+            //Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("PickDown"))
+        {
+            currentPoints -= 1;
+            other.gameObject.SetActive(false);
+            //Destroy(other.gameObject);
+        }
+        if (other.gameObject.CompareTag("Finish"))
+        {
+            WinCall();
+            other.gameObject.SetActive(false);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle")) { Respawn(); }
+    }
+
+    void Respawn()
+    {
+        transform.position = respawnPoint.position;
+    }
+
+    void UIUpdater()
+    {
+        pointsText.text = "Points: " + currentPoints.ToString() + "/" + winPoints.ToString();
+    }
+
+    void WinCall()
+    {
+        //Acción del cambio de escena
+        sceneManagerScript.SceneLoader(sceneToLoad);
+    }
+}
